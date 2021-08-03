ac2b757f9be17a18c8100af3acfbf78139a9a9ce
Marycorreia12 <correia_nety@hotmail.com>
2021-08-03 12:22:05 +0100

    Aula2

---
+++ Aula2/index2.js
@@ -0,0 +1,22 @@
+//Estou escrevendo um comentário
+console.log('Olá mundo');//outro  comentário
+
+
+console.log(1);
+console.log(2);
+console.log(3);
+
+console.log('------------------');
+
+console.log(1);
+//console.log(2);
+console.log(3);
+/*
+um bloco de código
+Linha de comentário
+*/
+/*
+console.log(1);
+console.log(2);
+console.log(3);
+*/
