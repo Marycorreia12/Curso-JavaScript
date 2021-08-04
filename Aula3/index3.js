dcdc1550a52a3b6f77edf761e88f05b1fa28a780
Marycorreia12 <correia_nety@hotmail.com>
2021-08-04 12:03:31 +0100

    Aula3

---
+++ Aula3/index3.js
@@ -0,0 +1,9 @@
+console.log('Oi');
+console.log('Maria');
+console.log('Este trecho será exibido no console do navegador');
+
+document.getElementById("demo").innerHTML=5+6;
+
+document.write(4+4);
+
+alert('Maria');
