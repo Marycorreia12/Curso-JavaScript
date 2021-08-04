9f8b4866e4c611a2172d382178ed1b2579110e9c
Marycorreia12 <correia_nety@hotmail.com>
2021-08-04 13:44:13 +0100

    Variável let

---
+++ Aula4/variaveis.js
@@ -0,0 +1,21 @@
+let nome; //Declarando a variável 
+nome = 'João';//Inicializando a variável
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome,'se chama Eduardo');
+
+//Invalid or unexpected token
+//let 1nome = 'Maria';
+
+//Unexpected token '-'
+//let meu-nome = 'maria';
+
+//ReferenceError: meu_nome is not defined
+//console.log(meu_nome);
+
+let nomeCliente = 'Maria';//cameCase
+let nomecliente = 'Santos';//Case-Sensitive
+console.log(nomeCliente, nomecliente);
