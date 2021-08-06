d7dadcfed41f3dbb45935a641faf6ca5b3a0ab9c
Marycorreia12 <correia_nety@hotmail.com>
2021-08-06 13:16:44 +0100

    alert, prompt e confirm

---
+++ Aula7/alert.js
@@ -0,0 +1,25 @@
+
+//confirm
+let txt;
+let r = confirm("Press a button!");
+if (r == true) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+}
+
+document.getElementById("testeConfirm").innerHTML = txt;
+
+
+//prompt
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+
+document.getElementById("testePrompt").innerHTML = resultado;
+
+//alert
+alert(' O resultado da sua conta foi: ' + resultado);
